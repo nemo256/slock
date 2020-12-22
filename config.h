65d9a866f9283b9b59f2fd4c929097c848435967
nemo256 <neggazimedlamine@gmail.com>
2020-12-22 20:04:45 +0100

    Updated my fork of slock!

--- config.h
+++ config.h
@@ -4,8 +4,8 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "blue",   /* during input */
+	[FAILED] = "red",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
